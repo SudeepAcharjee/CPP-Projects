c8984f6986cc8179d2731add1bdf15668fc3d713
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-07-23 13:37:56 +0530

    Second prog added

---
+++ Programs/program2.cpp
@@ -0,0 +1,10 @@
+//New Lines
+//To insert a new line, you can use the \n character:
+ #include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World! \n\n";
+  cout << "I am learning C++";
+  return 0;
+}
