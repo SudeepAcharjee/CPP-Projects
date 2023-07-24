481ef4162fc6d1a8ccc4e8de101aa19f41ac740e
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-07-24 22:09:22 +0530

    Assignment Operators

---
+++ Programs/assignmentoperator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+    int main()
+    {
+        int x;
+        cout << "Enter Your Number= ";
+        cin >> x;
+
+        x += 3;
+        cout << "The output of += operator = " << x;
+        return 0;
+    }
