cc1c72888a2b1ec916934ec076537f7e4e604ee5
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-07-25 10:22:26 +0530

    Assignment Operators

--- Programs/assignmentoperator.cpp
+++ Programs/assignmentoperator.cpp
@@ -3,11 +3,24 @@ using namespace std;
 
     int main()
     {
-        int x;
+        int x, y, z;
         cout << "Enter Your Number= ";
         cin >> x;
 
         x += 3;
         cout << "The output of += operator = " << x;
+
+
+
+        cout << "\nEnter a number= ";
+        cin >> y;
+        y -= 1;
+        cout << "The output of -= operator = " << y;
+
+
+        cout << "\nEnter a number = ";
+        cin >> z;
+        z *= 5 ;
+        cout << "The output of *= operator = " << z;
         return 0;
     }
