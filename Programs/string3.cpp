3bd293daa640afc5b0fde9ff50bed7756223abe0
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-07-27 14:39:53 +0530

    string3

---
+++ Programs/string3.cpp
@@ -0,0 +1,14 @@
+//string number concentration
+//"10" + "20" = "1020"
+#include<iostream>
+#include<string>
+using namespace std;
+    int main()
+    {
+        string a = "10";
+        string b ="20";
+        string c = a + b; //using string as concentrartion
+        cout << c;
+
+        return 0;
+    }
