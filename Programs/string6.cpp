2e1f6214ad2b35b6f11b1c3f49a13ad256c0f6e9
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-07-27 15:26:10 +0530

    Added

---
+++ Programs/string6.cpp
@@ -0,0 +1,19 @@
+//User Input string
+#include<iostream>
+#include<string>
+using namespace std;
+
+    int main()
+    { 
+        string a, b;
+        cout << "Enter your Age ";
+        getline (cin, b);
+        cout <<"Your age is " << b;
+
+        cout << "\nEnter Your name ";
+        cin >> a;
+        cout << " Your name is " << a;
+
+        return 0;
+
+    }
---
+++ Programs/string7.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+    int main()
+    {
+        std:: string a;
+        std:: cout << "Enter Your name ";
+        std:: cin >> a;
+        std:: cout << "Your name is " << a;
+
+
+        return 0;
+    }
