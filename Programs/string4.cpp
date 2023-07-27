cb20d69ef5594ffd1b4f75b3e0eab970d9f8127d
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-07-27 14:50:57 +0530

    string4

---
+++ Programs/string4.cpp
@@ -0,0 +1,14 @@
+//couting number of letters using in a variable
+#include<iostream>
+#include<string>
+using namespace std;
+    int main()
+    {
+        string a = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        cout << "Tthe lenght off the txt is " <<a.length(); //using the length function
+
+        string b = "Sudeep";
+        cout << "\nThe lenght of the txt is " <<b.size(); //using the size function
+
+        return 0;
+    }
