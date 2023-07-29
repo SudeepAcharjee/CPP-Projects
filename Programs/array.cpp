36ef01e1a052683ac0114e4711a991ebefd7e727
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-07-29 12:44:23 +0530

    Add

---
+++ Programs/array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+ int main()
+    {
+         string cars[5] = {"Volvo", "BMW", "Ford", "Mazda"};
+         cout << cars[2];
+         cout << "\nEnter a car name to add ";
+         cin >> cars[4];
+         cout << cars[4];
+         return 0;
+    
+    
+    
+    }
