a24ac4d1abe19510136971df19b2dd28c9bc20dc
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-07-31 13:30:42 +0530

    Added

---
+++ Programs/structture.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+    int main(){
+        struct{
+            string brand;
+            string model;
+            int year;
+        }myCar1, myCar2;
+
+
+  // Put data into the first structure
+  myCar1.brand = "BMW";
+  myCar1.model = "X5";
+  myCar1.year = 1999;
+
+  // Put data into the second structure
+  myCar2.brand = "Ford";
+  myCar2.model = "Mustang";
+  myCar2.year = 1969;
+
+  // Print the structure members
+  cout << myCar1.brand << " " << myCar1.model << " " << myCar1.year << "\n";
+  cout << myCar2.brand << " " << myCar2.model << " " << myCar2.year << "\n";
+  return 0;
+}
+    
