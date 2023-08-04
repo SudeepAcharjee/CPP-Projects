afb57d53c5e6ec7b8006fc47a93b7b04cc40f8b0
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-08-04 21:32:33 +0530

    Done

---
+++ C++Programs/fn-ref.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void function(int a){
+cout << "The numbers called are " << a << "\n";
+}
+    int main ()
+    {
+        function(1);
+        function(2);
+        function(3);
+        function(4);
+        function(5);
+        return 0;
+    }
