539a318c0c50c2e384810b583eef8c16fd2341a4
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-08-04 22:30:30 +0530

    Done

--- C++Programs/fn-ref.cpp
+++ C++Programs/fn-ref.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 using namespace std;
-void function(int a=10){
-cout << "The numbers called are " << a << "\n";
+void function(string name= "ram", int a=10 )//Also an example of multiple parameters
+{
+cout << "The numbers called are " << a << "  "<< name <<"\n";
 }
     int main ()
     {
-        function(1);
-        function(2);
-        function(3);
-        function(4);
-        function(5);
+        function( "Sudeep", 1);
+        function("Dev", 2);
+        function("Ritu", 3);
+        function("David", 4);
+        function("Rohit", 5);
         function();
         return 0;
     }
