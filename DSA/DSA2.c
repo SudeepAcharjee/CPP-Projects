3700ed3691d94581f684859b11432d0d48799da3
SudeepAcharjee <sudeepacharjeegp@gmail.com>
2023-08-14 20:13:07 +0530

    Added

---
+++ DSA/DSA2.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+/*Explain :
+Alright, imagine you have a box called "Node." This box has two compartments: one for holding a number (we call it "data"),
+ and another compartment for pointing to another box of the same kind (we call this "next").
+So, each "Node" box can hold a number and show where the next "Node" box is. It's like a chain of boxes where each box knows about the next box.
+For example, if you have a line of these boxes, the first box can hold a number and show to the second box. 
+The second box can hold a number and show to the third box, and so on. This way, you can create a line of boxes, each holding a number and pointing to the next box in the line. That's how a linked list works!*/
+
+
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the beginning of the list
+void insertAtBeginning(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    newNode->next = *head;
+    *head = newNode;
+}
+
+// Function to insert a node at the end of the list
+void insertAtEnd(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = newNode;
+}
+
+// Function to print the list
+void printList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to free the memory occupied by the list
+void freeList(struct Node* head) {
+    while (head != NULL) {
+        struct Node* temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct Node* head = NULL; // Initialize an empty list
+
+    // User input and menu
+    int choice, value;
+    do {
+        printf("\nSingly Linked List Operations:\n");
+        printf("1. Insert at Beginning\n");
+        printf("2. Insert at End\n");
+        printf("3. Print List\n");
+        printf("4. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter value to insert: ");
+                scanf("%d", &value);
+                insertAtBeginning(&head, value);
+                break;
+            case 2:
+                printf("Enter value to insert: ");
+                scanf("%d", &value);
+                insertAtEnd(&head, value);
+                break;
+            case 3:
+                printf("List: ");
+                printList(head);
+                break;
+            case 4:
+                // Clean up memory before exiting
+                freeList(head);
+                printf("Exiting program.\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    } while (choice != 4);
+
+    return 0;
+}
