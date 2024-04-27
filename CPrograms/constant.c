b745868a3620c3f958c8d5dd6b00ca5aaba54c2d
Sudeep Acharjee <sudeepacharjee@Sudeeps-MacBook-Air.local>
2024-04-27 11:50:53 +0530

    Updated Code

--- CPrograms/constant.c
+++ CPrograms/constant.c
@@ -2,7 +2,7 @@
 int main()
     {
         int a = 10,/*use of Constant*/ b, c;
-        printf ("Enter a number to get its multiply by 10 ");
+        printf ("Enter a number to get its multiply by 10= ");
         scanf("%d", &b);
         c =a*b;
         printf("The number is %d", c);
